571a5e397022e8695d390f9a668021a278fefcd9
luxick <git@luxick.de>
2023-06-08 09:47:07 +0200

    Add HTMX

---
+++ Homepage/Controllers/ToolsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Homepage.Controllers;
+
+public class ToolsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
